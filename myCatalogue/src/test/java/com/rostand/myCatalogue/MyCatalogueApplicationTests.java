416840ffd12280e31d711eb3a2011cdf2572bd1f
siewe-rostand <rostand.siewe9@gmail.com>
2021-03-02 11:31:33 +0100

    spring projects

---
+++ myCatalogue/src/test/java/com/rostand/myCatalogue/MyCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostand.myCatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
